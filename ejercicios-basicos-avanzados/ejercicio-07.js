9bb36fce302c28c8e072518d7ef8cb4bb1706ebf
Jesús <jhernandezrexa@gmail.com>
2026-02-24 18:06:14 +0100

    Ejercicio 7 terminado.

---
+++ ejercicios-basicos-avanzados/ejercicio-07.js
@@ -0,0 +1,9 @@
+/*
+Completa esta función para que, al recibir dos números por argumento, te devuelva por consola el más alto de los dos.
+}
+
+*/
+
+function greaterNumber(numberOne, numberTwo) {
+  console.log(Math.max(numberOne, numberTwo));
+}
